90014c661e3c7495f62a46df207ad0b0db072d10
piatee <mateopiattelli33@gmail.com>
2025-07-18 23:12:46 -0300

    enemies

--- Assets/Scripts/Enemies/EnemyBehaviour.cs
+++ Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -10,9 +10,43 @@ public class EnemyBehaviour : MonoBehaviour
     SpriteRenderer sr;
     int dir = -1;
 
+    public Transform Character;              // El personaje principal
+    public float attackRange = 2f;        // Rango para detenerse y atacar
+    public float attackInterval = 1.5f;   // Tiempo entre ataques
+    public Transform CharacterCheck;
+    public LayerMask CharacterLayer;
+    public bool stop;
+
+    private float attackTimer = 0f;
+    private bool playerInRange = false;
+
+
     private void Start()
     {
-      
+        
+        if (AttackRange())
+        {
+            attackTimer += Time.deltaTime * EntityData.gameData.currentGameSpeed;
+
+
+            if (attackTimer >= attackInterval)
+            {
+                Attack();
+                attackTimer = 0f;
+            }
+        }
+        void Attack()
+        {
+            // Acá va lo que hace el ataque: daño, animación, etc.
+            Debug.Log("Enemigo ataca con su espada");
+        }
+    }
+    private bool AttackRange()
+    {
+        Debug.Log("en rango");
+
+        return Physics2D.OverlapCircle(transform.position + new Vector3(dir, 0 ,0), 0.5f, CharacterLayer);
+        
     }
 
     public EnemyBehaviour Initialize(ATTACKANGLE side)
@@ -30,9 +64,13 @@ public class EnemyBehaviour : MonoBehaviour
 
     void Update()
     {
-        if(Vector2.Distance(transform.position, player.transform.position) > 1)
+        if(!stop)
         {
             transform.position += (Vector3.right * dir) * (EntityData.EnemyMelee.speed * EntityData.gameData.currentGameSpeed) * Time.deltaTime;
+        }
+        if (AttackRange())
+        {
+            stop = true;
         }
     }
 }
